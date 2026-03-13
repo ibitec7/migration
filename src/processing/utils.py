c79330aa1e11a39ef94a88fff1e4f3e36a563d94
Syed Ibrahim Omer <syed.ibrahim.omer.2@gmail.com>
2026-03-13 17:56:17 +0800

    Modified utilities

--- src/processing/utils.py
+++ src/processing/utils.py
@@ -1,4 +1,20 @@
 import logging
+import multiprocessing
+
+MONTHS_MAP = {
+        "JAN": 1,
+        "FEB": 2,
+        "MAR": 3,
+        "APR": 4,
+        "MAY": 5,
+        "JUN": 6,
+        "JUL": 7,
+        "AUG": 8,
+        "SEP": 9,
+        "OCT": 10,
+        "NOV": 11,
+        "DEC": 12
+    }
 
 def setup_logger(log_file, log_level=logging.INFO, write_console=True, write_file=True) -> logging.Logger:
     logger = logging.getLogger(__name__)
@@ -17,3 +33,12 @@ def setup_logger(log_file, log_level=logging.INFO, write_console=True, write_fil
         logger.addHandler(console_handler)
 
     return logger
+
+def get_optimal_process_count() -> int:
+    """
+    Calculate optimal process count for CPU-bound PDF processing.
+    ProcessPoolExecutor uses true parallelism with multiprocessing (avoids GIL).
+    """
+    cpu_count = multiprocessing.cpu_count()
+    optimal_procs = max(2, cpu_count)
+    return optimal_procs
