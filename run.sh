c4ed3da0c1d842ec6573287a31b9743fc12759b1
Syed Ibrahim Omer <syed.ibrahim.omer.2@gmail.com>
2026-03-24 21:27:28 +0800

    Updated script to download data.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source .venv/bin/activate
+hf download sdsc2005-migration/flan-t5-tensorrt-int8_wo-engine --local-dir src/models/tensor-rt
 python src/collection/visa.py && python src/collection/encounter.py && bash download_trends.sh && python src/processing/parse.py
